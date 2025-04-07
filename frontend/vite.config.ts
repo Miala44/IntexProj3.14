92631332697acefbd20e908c0ff1a2b31a1725b5
Miala44 <adamsmia7@gmail.com>
2025-04-07 09:50:01 -0600

    Initial commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    headers: {
+      'Content-Security-Policy':
+        "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://localhost:5000; img-src 'self' data: https://localhost:5000; style-src 'self' 'unsafe-inline' fonts.googleapis.com; frame-src 'self' https://localhost:5000; font-src 'self' fonts.gstatic.com data",
+    },
   },
 });
