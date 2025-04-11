6756cdab248e34ceb6ad868fd04cf2e1261174d9
Miala44 <adamsmia7@gmail.com>
2025-04-10 22:06:51 -0600

    backend updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
+// import { CartProvider } from './context/CartContext';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MoviesPage from './pages/MoviePage';
@@ -15,22 +15,20 @@ import 'react-multi-carousel/lib/styles.css';
 function App() {
   return (
     <>
-      <CartProvider>
-        <Router>
-          <Routes>
-            <Route path="/moviepage" element={<IntroPage />} />
-            <Route path="/intro" element={<IntroPage />} />
-            <Route path="/movie" element={<MoviesPage />} />
-            <Route path="/detailPage/:id" element={<DetailPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/privacyPage" element={<PrivacyPage />} />
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/recommendations" element={<RecommendationsPage />} />
-            <Route path="/adminmovies" element={<AdminPage />} />
-          </Routes>
-        </Router>
-      </CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/moviepage" element={<IntroPage />} />
+          <Route path="/intro" element={<IntroPage />} />
+          <Route path="/movie" element={<MoviesPage />} />
+          <Route path="/detailPage/:id" element={<DetailPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/privacyPage" element={<PrivacyPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/recommendations" element={<RecommendationsPage />} />
+          <Route path="/adminmovies" element={<AdminPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
